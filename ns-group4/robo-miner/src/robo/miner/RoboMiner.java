5c14243c4250cff3f55f65e0879b8c78b7dbcd75
Tiago Varela <vogait@gmail.com>
2012-09-26 21:23:00 +0100

    Added examples and modifed one of them.

--- ns-group4/robo-miner/src/robo/miner/RoboMiner.java
+++ ns-group4/robo-miner/src/robo/miner/RoboMiner.java
@@ -7,7 +7,7 @@ public class RoboMiner {
     
     public static void main(String[] args) {
         Board b = new Board();
-        b.createFromFile("files/example1.map");
+        b.createFromFile("files/example1.1.map");
         System.out.println(b.drawMap());
         
         //2 iteração
