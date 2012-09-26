2b54ced076c082f0a297986a6f7216f468e17ac1
Tiago Varela <vogait@gmail.com>
2012-09-26 19:57:59 +0100

    Added create from file in main class. Added example files.

--- ns-group4/robo-miner/src/robo/miner/RoboMiner.java
+++ ns-group4/robo-miner/src/robo/miner/RoboMiner.java
@@ -1,19 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package robo.miner;
 
-/**
- *
- * @author tiago
- */
 public class RoboMiner {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Board b = new Board();
+        b.createFromFile("files/example1.map");
     }
 }
