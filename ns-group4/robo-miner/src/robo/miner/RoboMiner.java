f8d8c816c9e1edd3f1422e628b90fcc8bcc305d8
Tiago Varela <vogait@gmail.com>
2012-09-26 19:32:53 +0100

    Initial source commit.

--- ns-group4/robo-miner/src/robo/miner/RoboMiner.java
+++ ns-group4/robo-miner/src/robo/miner/RoboMiner.java
@@ -2,13 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package robot.miner;
+package robo.miner;
 
 /**
  *
  * @author tiago
  */
-public class RobotMiner {
+public class RoboMiner {
 
     /**
      * @param args the command line arguments
